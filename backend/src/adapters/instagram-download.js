81a7803a28d67052794d0eae7c497f1eba50ff21
Sage Drake <sagedrake11@gmail.com>
2024-12-19 16:36:15 -0500

    Test connecting to instagram using puppeteer

--- backend/src/adapters/instagram-download.js
+++ backend/src/adapters/instagram-download.js
@@ -1,30 +1,71 @@
 import puppeteer from "puppeteer";
 
-(async () => {
-	// Launch the browser and open a new blank page
-	const browser = await puppeteer.launch();
-	const page = await browser.newPage();
+export const INSTAGRAM_URL = "https://www.instagram.com/";
+const SCREEN_WIDTH = 1080;
+const SCREEN_HEIGHT = 1024;
 
-	// Navigate the page to a URL
-	await page.goto("https://developer.chrome.com/");
+let browser;
 
-	// Set screen size
-	await page.setViewport({ width: 1080, height: 1024 });
+// connect to instagram using puppeteer and return page
+// if this function fails, it's a sign that internet may be down or instagram may be down
+export async function connectToInstagram() {
+	try {
+		browser = await puppeteer.launch();
+		const page = await browser.newPage();
 
-	// Type into search box
-	await page.type(".devsite-search-field", "automate beyond recorder");
+		await page.goto(INSTAGRAM_URL);
 
-	// Wait and click on first result
-	const searchResultSelector = ".devsite-result-item-link";
-	await page.waitForSelector(searchResultSelector);
-	await page.click(searchResultSelector);
+		await page.setViewport({ width: SCREEN_WIDTH, height: SCREEN_HEIGHT });
 
-	// Locate the full title with a unique string
-	const textSelector = await page.waitForSelector("text/Customize and automate");
-	const fullTitle = await textSelector?.evaluate((el) => el.textContent);
+		return Promise.resolve(page);
+	} catch (e) {
+		return Promise.reject(new Error(`Unexpected error when connecting to instagram: ${e}`));
+	}
+}
 
-	// Print the full title
-	console.log('The title of this blog post is "%s".', fullTitle);
+export async function disconnectFromInstagram() {
+	try {
+		await browser.close();
+		return Promise.resolve();
+	} catch(e) {
+		return Promise.reject(new Error(`Error when closing browser: ${e}`));
+	}
+}
 
-	await browser.close();
-})();
+// login to instagram on the provided page, which whould be on instagram.com already
+function loginToInstagram(page) {}
+
+// navigate to the user's saved posts
+function enterSavedPosts(page) {}
+
+// return [<username>, <description>] for now (will get image too at some point)
+function getPostInfo(page) {}
+
+// (async () => {
+// 	// Launch the browser and open a new blank page
+// 	const browser = await puppeteer.launch();
+// 	const page = await browser.newPage();
+
+// 	// Navigate the page to a URL
+// 	await page.goto("https://developer.chrome.com/");
+
+// 	// Set screen size
+// 	await page.setViewport({ width: 1080, height: 1024 });
+
+// 	// Type into search box
+// 	await page.type(".devsite-search-field", "automate beyond recorder");
+
+// 	// Wait and click on first result
+// 	const searchResultSelector = ".devsite-result-item-link";
+// 	await page.waitForSelector(searchResultSelector);
+// 	await page.click(searchResultSelector);
+
+// 	// Locate the full title with a unique string
+// 	const textSelector = await page.waitForSelector("text/Customize and automate");
+// 	const fullTitle = await textSelector?.evaluate((el) => el.textContent);
+
+// 	// Print the full title
+// 	console.log('The title of this blog post is "%s".', fullTitle);
+
+// 	await browser.close();
+// })();
--- backend/test/adapters/instagram-download.spec.js
+++ backend/test/adapters/instagram-download.spec.js
@@ -5,3 +5,19 @@
 // test that we can get to saved posts
 // test that correct post information gets downloaded
 // test usage of AI tools for getting recipe title, ingredients, and instructions
+
+import { expect } from "chai";
+import { connectToInstagram, disconnectFromInstagram, INSTAGRAM_URL } from "../../src/adapters/instagram-download.js";
+
+describe("Instagram", function () {
+	it("Should successfully connect to instagram site", async function () {
+		try {
+			let page = await connectToInstagram();
+			const url = page.url();
+			expect(url).to.equal(INSTAGRAM_URL);
+			await disconnectFromInstagram();
+		} catch (e) {
+			expect.fail(e);
+		}
+	});
+});
