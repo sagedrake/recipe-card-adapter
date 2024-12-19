909412acf97f15375692c13bb3c6cec989767a19
Sage Drake <sagedrake11@gmail.com>
2024-12-19 11:19:54 -0500

    Add puppeteer for getting instagram recipes

---
+++ backend/test/adapters/instagram-download.spec.js
@@ -0,0 +1,7 @@
+// tests for downloading saved posts from instagram
+
+// test that instagram can be connected to
+// test that account can be signed into
+// test that we can get to saved posts
+// test that correct post information gets downloaded
+// test usage of AI tools for getting recipe title, ingredients, and instructions
